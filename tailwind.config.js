823176f6dca690cf2959b0fa998a6c3ce38d3f2f
Noval Akbar Ramadhany <novalarama@Novals-MacBook-Air.local>
2024-08-30 19:00:46 +0700

    First Commit-Motion Practice

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
